36f13d2837e6fd54b76289011471a2aee0a1eb70
sertony <serkov_anton@mail.ru>
2019-09-20 14:48:36 +0300

    fix web crash

--- src/api/hoc.js
+++ src/api/hoc.js
@@ -2,63 +2,63 @@ import * as React from 'react';
 import { Animated } from 'react-native';
 
 export function scaleable(Node, animatedValue) {
-  return (
-    <Animated.View
-      style={{
-        transform: [
-          {
-            scale: animatedValue.interpolate({
-              inputRange: [-0, 50],
-              outputRange: [1.5, 1],
-              extrapolateRight: 'clamp'
-            })
-          }
-        ]
-      }}
-    >
-      {Node}
-    </Animated.View>
-  );
+    return (
+        <Animated.View
+            style={{
+                transform: [
+                    {
+                        scale: animatedValue.interpolate({
+                            inputRange: [-0, 50],
+                            outputRange: [1.5, 1],
+                            extrapolateRight: 'clamp',
+                        }),
+                    },
+                ],
+            }}
+        >
+            {Node}
+        </Animated.View>
+    );
 }
 
 export function disappear(
-  Node,
-  animatedValue,
-  start,
-  end,
-  shouldDisappear = true
+    Node,
+    animatedValue,
+    start,
+    end,
+    shouldDisappear = true,
 ) {
-  if (!shouldDisappear) return Node;
-  return (
-    <Animated.View
-      style={{
-        opacity: animatedValue.interpolate({
-          inputRange: [start, end],
-          outputRange: [1, 0],
-          extrapolate: 'clamp'
-        })
-      }}
-    >
-      {Node}
-    </Animated.View>
-  );
+    if (!shouldDisappear) return Node;
+    return (
+        <Animated.View
+            style={{
+                opacity: animatedValue.interpolate({
+                    inputRange: [start, end],
+                    outputRange: [1, 0],
+                    extrapolate: 'clamp',
+                }),
+            }}
+        >
+            {Node}
+        </Animated.View>
+    );
 }
 
 export function appear(Node, animatedValue, start, end, shouldAppear = true) {
-  if (!shouldAppear) return Node;
-  return (
-    <Animated.View
-      pointerEvents="box-none"
-      style={{
-        zIndex: 9999,
-        opacity: animatedValue.interpolate({
-          inputRange: [start, end],
-          outputRange: [0, 1],
-          extrapolate: 'clamp'
-        })
-      }}
-    >
-      {Node}
-    </Animated.View>
-  );
+    if (!shouldAppear) return Node;
+    return (
+        <Animated.View
+            pointerEvents="box-none"
+            style={{
+                zIndex: 9999,
+                opacity: animatedValue.interpolate({
+                    inputRange: [start, end],
+                    outputRange: [0, 1],
+                    extrapolate: 'clamp',
+                }),
+            }}
+        >
+            {Node}
+        </Animated.View>
+    );
 }
